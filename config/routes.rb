58f77a759d181e35de08c120f261f90c0bd0fcc5
coockie2 <adapter_yu@yahoo.co.jp>
2025-04-15 00:45:45 +0900

    task 機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # refs: https://railsguides.jp/routing.html#rails%E3%83%AB%E3%83%BC%E3%82%BF%E3%83%BC%E3%82%92%E8%A8%AD%E5%AE%9A%E3%81%99%E3%82%8B
+  resources :tasks
 end
