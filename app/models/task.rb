ad30cd20d825cfc60bf07b4175a10d75f0f58464
yKuki <adapter_yu@yahoo.co.jp>
2025-07-05 02:39:12 +0900

    バリデーションエラーを画面に表示するように修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,15 @@
 class Task < ApplicationRecord
-  validates :name, presence: true
-  validates :description, presence: true, length: {minimum: 10}
+  before_validation :set_nameless_name
+  validates :name, presence: true, length: {maximum: 30}
+  validate :validate_name_not_include_comma
+
+  private
+
+  def set_nameless_name
+    self.name = "名前なし" if name.blank?
+  end
+
+  def validate_name_not_include_comma
+    errors.add(:name, "にカンマを含めることはできません") if name&.include?(',')
+  end
 end
