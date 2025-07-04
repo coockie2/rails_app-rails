225782c8eaa97e62a419b3408abd1f0c2ac109e3
yKuki <adapter_yu@yahoo.co.jp>
2025-07-05 01:51:07 +0900

    もろもろ更新

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
+// app/assets/config/manifest.js
 //= link_tree ../images
-//= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link application.js
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
---
+++ app/javascript/application.js
@@ -0,0 +1,5 @@
+import "@hotwired/turbo-rails"
+import Rails from "@rails/ujs"
+Rails.start()
+
+console.log("JS loaded via importmap")
